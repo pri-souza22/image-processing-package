a57715fa8be4c3b9dacabb1b727148ba20de6ad2
pri-souza22 <priscilamsouza1826@gmail.com>
2025-04-04 20:17:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name="image_processing_package",
+    name="image_processing_package_test",
     version="0.0.1",
     author="Priscila Souza",
     author_email="priscilamsouza1826@gmail.com",
     description="Um pacote para processamento de imagens.",
-    long_description=open("README.md").read(),
+    long_description=page_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pri-souza22/image-processing-package",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "opencv-python",
-        "matplotlib"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License : MIT ",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    install_requires=requirements
+    python_requires=">=3.8",
 )
