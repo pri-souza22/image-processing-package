6689072578ecf063a5bfbde7c1f78d11107d076c
pri-souza22 <priscilamsouza1826@gmail.com>
2025-04-04 18:57:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License : MIT ",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
